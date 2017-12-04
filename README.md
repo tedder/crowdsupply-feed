c8cf8b5d7aa6b4cdcf402eca2b3bb9a31cea35f7
tedder <ted.timmons@stansonhealth.com>
2017-12-04 14:13:29 -0800

    firsties

---
+++ README.md
@@ -0,0 +1,5 @@
+crowdsupply.com product feeds, aka 'rss feeds'.
+
+The main 'browse' feed: https://dyn.tedder.me/rss/crowdsupply-browse.json
+
+MIT licensed.
---
+++ scrape-browse.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+# MIT license, see /LICENSE
+
+import boto3
+import json
+import requests
+import lxml
+import lxml.html
+
+# using the JSON feed spec: https://jsonfeed.org/version/1
+
+SITE_URL = 'https://www.crowdsupply.com'
+BROWSE_URL = SITE_URL + '/browse'
+S3_KEY = 'rss/crowdsupply-browse.json'
+FEED_URL = 'https://dyn.tedder.me/' + S3_KEY
+
+
+def get_only_one(item):
+  if len(item) != 1:
+    raise Exception("we wanted one item but instead got {} of them.".format(len(item)))
+  return item[0]
+
+
+def parse_tile(tile):
+  ret = {}
+  ret['url'] = SITE_URL + tile.get('href')
+  ret['id'] = tile.get('href')
+  ret['image'] = SITE_URL + tile.xpath('img')[0].get('src')
+  overview = get_only_one(tile.xpath("div[contains(@class, 'project-tile-overview')]"))
+  ret['title'] = get_only_one(overview.xpath("h3")).text
+
+  overview_text = []
+  for opara in overview.xpath("p"):
+    overview_text.append(opara.text)
+  ret['content_html'] = "<br>\n".join(overview_text)
+
+  return ret
+
+
+r = requests.get(BROWSE_URL)
+browse_text = r.text
+print("site size: {}".format(len(browse_text)))
+tree = lxml.html.fromstring(browse_text)
+
+browse_container = tree.xpath("//section[contains(@class, 'section-browse')]//a[contains(@class, 'project-tile')]")
+
+jfeed = {
+  'version': 1,
+  'title': 'Crowd Supply projects',
+  'home_page_url': BROWSE_URL,
+  'description': 'Projects scraped from the crowdsupply browse page',
+  'user_comment': 'If dates/creator/funding level is needed, I can scrape the child pages too. Probably worth caching if so.',
+  'author': {'name': 'tedder', 'url': 'https://tedder.me/' },
+  'feed_url': FEED_URL,
+  'items': [],
+}
+for tile in browse_container:
+  tileinfo = parse_tile(tile)
+  jfeed['items'].append(tileinfo)
+
+feedj = json.dumps(jfeed)
+
+s3 = boto3.client('s3')
+s3.put_object(
+  ACL='public-read',
+  Body=feedj,
+  Bucket='dyn.tedder.me',
+  Key=S3_KEY,
+  ContentType='application/json',
+  CacheControl='public, max-age=300' # todo: 3600
+)
+
+
+
